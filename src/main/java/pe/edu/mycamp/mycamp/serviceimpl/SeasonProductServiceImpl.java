3bf2782589d36f3572fcec29def09d5b0393b66c
Diego Torres <56986122+DiegoTorres98@users.noreply.github.com>
2023-06-20 09:19:16 -0500

    diego

---
+++ src/main/java/pe/edu/mycamp/mycamp/serviceimpl/SeasonProductServiceImpl.java
@@ -0,0 +1,9 @@
+package pe.edu.mycamp.mycamp.serviceimpl;
+
+import org.springframework.stereotype.Service;
+import pe.edu.mycamp.mycamp.services.SeasonProductService;
+
+@Service
+public class SeasonProductServiceImpl implements SeasonProductService {
+
+}

39f6754fad6bc5ed1f7a1186c5fea48a349e31c3
Agonzales17 <107695197+Agonzales17@users.noreply.github.com>
2023-06-24 22:03:30 -0500

    Add files via upload

--- src/main/java/pe/edu/mycamp/mycamp/serviceimpl/SeasonProductServiceImpl.java
+++ src/main/java/pe/edu/mycamp/mycamp/serviceimpl/SeasonProductServiceImpl.java
@@ -1,8 +1,19 @@
 package pe.edu.mycamp.mycamp.serviceimpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pe.edu.mycamp.mycamp.entities.SeasonProduct;
+import pe.edu.mycamp.mycamp.repositories.SeasonProductRepository;
+import pe.edu.mycamp.mycamp.services.SeasonProductService;
+
+import java.util.List;
 
 @Service
 public class SeasonProductServiceImpl implements SeasonProductService {
-
+    @Autowired
+    private SeasonProductRepository seasonProductRepository;
+    @Override
+    public List<SeasonProduct> list() {
+        return seasonProductRepository.findAll();
+    }
 }
--- src/main/java/pe/edu/mycamp/mycamp/services/SeasonProductService.java
+++ src/main/java/pe/edu/mycamp/mycamp/services/SeasonProductService.java
@@ -1,4 +1,9 @@
 package pe.edu.mycamp.mycamp.services;
 
+import pe.edu.mycamp.mycamp.entities.SeasonProduct;
+
+import java.util.List;
+
 public interface SeasonProductService {
+    public List<SeasonProduct> list();
 }
